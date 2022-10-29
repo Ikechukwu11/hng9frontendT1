7c3e547282735c11f017069f7aa1e25173da0734
Ikechukwu.kev <kevinuzoma@gmail.com>
2022-10-29 11:42:04 +0100

    new file:   public/img/menu-icon-hover.svg 	new file:   public/img/menu-icon-mobile.svg 	new file:   public/img/menu-icon.svg 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'./img/profile_img.jpg'} id="profile_img" className="profile_img" alt="profile_img" />
-        <h3>
-         Kevin Uzoma
+        <img src={'./img/profile_img.jpg'} id="profile__img" className="profile__img" alt="profile_img" />
+        <h3 className='twitter'>
+         Kekesmovic
         </h3>
+        <h3 id='slack'>
+         Kekesmovic
+        </h3>
+
+        <div className='Menu'>
+        <img  src={'./img/menu-icon.svg'} id='menu-icon' className="menu-icon" alt="menu-icon" />
+        <img  src={'./img/menu-icon-hover.svg'} id='menu-icon-hover' className="menu-icon" alt="menu-icon-hover" />
+        <img  src={'./img/menu-icon-mobile.svg'} id='menu-icon-mobile' className="menu-icon" alt="menu-icon" />
+        </div>
+        
         <div className='App-link-div'>
         <a id='twitter'
           className="App-link"
@@ -20,7 +30,7 @@ function App() {
         </div>
 
         <div className='App-link-div'>
-        <a id='btn_zuri'
+        <a id='btn__zuri'
           className="App-link"
           href="https://training.zuri.team/"
           target="_blank"
@@ -42,7 +52,7 @@ function App() {
         </div>
 
         <div className='App-link-div'>
-        <a id='book_python'
+        <a id='book__python'
           className="App-link"
           href="https://books.zuri.team/python-for-beginners?ref_id=%3Ckekesmovic%3E"
           target="_blank"
@@ -64,7 +74,7 @@ function App() {
         </div>
         
         <div className='App-link-div'>
-        <a id='book_design'
+        <a id='book__design'
           className="App-link"
           href="https://books.zuri.team/design-rules"
           target="_blank"
@@ -93,6 +103,11 @@ function App() {
         </div>
         
       </header>
+      <footer className='App-footer'>
+        <img src={'./img/Zuri.Internship_Logo.png'} id="zuri_logo" className="zuri_logo" alt="zuri_logo" />
+        <p>HNG Internship 9 Frontend Task</p>
+        <img src={'./img/I4G.png'} id="hng_logo" className="hng_logo" alt="hng_logo" />
+      </footer>
     </div>
   );
 }
